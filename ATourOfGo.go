1c0790757a70097d9e48158cb2d2245c406dbd49
goat2130 <yagisawahodaka@team-lab.com>
2024-09-07 15:30:36 +0900

    first commit

---
+++ ATourOfGo.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"runtime"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+
+func main() {
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwiin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		fmt.Printf("%s.\n", os)
+	}
+}
