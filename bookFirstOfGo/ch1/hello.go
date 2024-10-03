9b956f2131e9e07d56d7f0da7c51bd2b2d2eedd7
goat2130 <yagisawahodaka@team-lab.com>
2024-10-03 23:50:19 +0900

    add: 変数

--- bookFirstOfGo/ch1/hello.go
+++ bookFirstOfGo/ch1/hello.go
@@ -2,9 +2,30 @@ package main
 
 import "fmt"
 
+const x int64 = 10
+
+const (
+	idKey   = "id"
+	nameKey = "name"
+	runeKey = 'a'
+)
+
+const z = 20 * 10
+
 func main() {
-	fmt.Println("Hello, World!")
-	var x int
-	x = 10
+	// fmt.Println("Hello, World!")
+	// var x int
+	// x = 10
+	// fmt.Println(x)
+
+	const y = "hello"
+
 	fmt.Println(x)
+	fmt.Println(y)
+
+	x = x + 10
+	y = "world"
+
+	fmt.Println(x)
+	fmt.Println(y)
 }
