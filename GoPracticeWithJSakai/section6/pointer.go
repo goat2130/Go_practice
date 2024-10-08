173dfd500509e0b9d845c715f6ce593d95c3c706
goat2130 <yagisawahodaka@team-lab.com>
2024-10-08 09:29:46 +0900

    add

--- GoPracticeWithJSakai/section6/pointer.go
+++ GoPracticeWithJSakai/section6/pointer.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func failedUpdate(px *int) {
 	x2 := 20
@@ -11,6 +14,33 @@ func update(px *int) {
 	*px = 20
 }
 
+type Counter struct {
+	total      int
+	lastUpdate time.Time
+}
+
+// ポインタレシーバー
+func (c *Counter) Increment() {
+	c.total++
+	c.lastUpdate = time.Now()
+}
+
+// 値レシーバー
+func (c Counter) String() string {
+	return fmt.Sprintf("{total: %d, lastUpdate: %v}", c.total, c.lastUpdate)
+}
+
+// ポインタレシーバーに値をコピーして渡す
+func doUpdateWrong(c Counter) {
+	c.Increment()
+	fmt.Println(c.String())
+}
+
+func doUpdate(c *Counter) {
+	c.Increment()
+	fmt.Println(c.String())
+}
+
 func main() {
 	x := 10
 	failedUpdate(&x)
@@ -18,4 +48,14 @@ func main() {
 
 	update(&x)
 	fmt.Println(x)
+
+	var counter Counter
+	fmt.Println(counter.String())
+
+	counter.Increment()
+	fmt.Println(counter.String())
+
+	doUpdateWrong(counter)
+
+	doUpdate(&counter)
 }
--- GoPracticeWithJSakai/section8/go.mod
+++ GoPracticeWithJSakai/section8/go.mod
@@ -1,3 +1,5 @@
 module GoPracticeWithJSakai/section8
 
 go 1.23.0
+
+require github.com/markcheno/go-talib v0.0.0-20190307022042-cd53a9264d70 // indirect
