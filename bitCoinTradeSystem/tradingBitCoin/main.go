9c28b3dba644433baa040cf727526b77e85726b9
goat2130 <yagisawahodaka@team-lab.com>
2024-10-15 20:47:07 +0900

    add: section103

--- bitCoinTradeSystem/tradingBitCoin/main.go
+++ bitCoinTradeSystem/tradingBitCoin/main.go
@@ -2,31 +2,31 @@ package main
 
 import (
 	"fmt"
-	"tradingBitCoin/bitflyer"
+	"tradingBitCoin/app/models"
 	"tradingBitCoin/config"
 	"tradingBitCoin/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+	//apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
 	//ticker, _ := apiClient.GetTicker("BTC_USD")
 	//fmt.Println(ticker)
 	//fmt.Println(ticker.GetMidPrice())
 	//fmt.Println(ticker.DateTime())
 	//fmt.Println(ticker.TruncateDateTime(time.Hour))
 
-	order := &bitflyer.Order{
-		ProductCode:     config.Config.ProductCode,
-		ChildOrderType:  "LIMIT",
-		Side:            "BUY",
-		Price:           7000,
-		Size:            0.01,
-		MinuteToExpires: 1,
-		TimeInForce:     "GTC",
-	}
-	res, _ := apiClient.SendOrder(order)
-	fmt.Println(res.ChildOrderAcceptanceID)
+	//order := &bitflyer.Order{
+	//	ProductCode:     config.Config.ProductCode,
+	//	ChildOrderType:  "LIMIT",
+	//	Side:            "BUY",
+	//	Price:           7000,
+	//	Size:            0.01,
+	//	MinuteToExpires: 1,
+	//	TimeInForce:     "GTC",
+	//}
+	//res, _ := apiClient.SendOrder(order)
+	//fmt.Println(res.ChildOrderAcceptanceID)
 
 	//i := "JRF20181012-144016-140584"
 	//params := map[string]string{
@@ -35,4 +35,6 @@ func main() {
 	//}
 	//r, _ := apiClient.ListOrder(params)
 	//fmt.Println(r)
+
+	fmt.Println(models.DbConnection)
 }
