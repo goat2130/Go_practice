8ed25addce699ee3ca8bfb316e32a08e00a7ee71
goat2130 <yagisawahodaka@team-lab.com>
2024-10-17 15:53:21 +0900

    127

--- bitCoinTradeSystem/tradingBitCoin/app/models/events.go
+++ bitCoinTradeSystem/tradingBitCoin/app/models/events.go
@@ -6,23 +6,24 @@ import (
 	"log"
 	"strings"
 	"time"
+
 	"tradingBitCoin/config"
 )
 
 type SignalEvent struct {
-	ProductCode string    `json:"product_code"`
 	Time        time.Time `json:"time"`
+	ProductCode string    `json:"product_code"`
 	Side        string    `json:"side"`
 	Price       float64   `json:"price"`
 	Size        float64   `json:"size"`
 }
 
 func (s *SignalEvent) Save() bool {
-	cmd := fmt.Sprintf("INSERT INTO %s (time, product_code, side, prie, size) VALUES (?, ?, ?, ?, ?)", tableNameSignalEvents)
+	cmd := fmt.Sprintf("INSERT INTO %s (time, product_code, side, price, size) VALUES (?, ?, ?, ?, ?)", tableNameSignalEvents)
 	_, err := DbConnection.Exec(cmd, s.Time.Format(time.RFC3339), s.ProductCode, s.Side, s.Price, s.Size)
 	if err != nil {
 		if strings.Contains(err.Error(), "UNIQUE constraint failed") {
-			log.Fatal(err)
+			log.Println(err)
 			return true
 		}
 		return false
@@ -40,11 +41,10 @@ func NewSignalEvents() *SignalEvents {
 
 func GetSignalEventsByCount(loadEvents int) *SignalEvents {
 	cmd := fmt.Sprintf(`SELECT * FROM (
-    SELECT time, product_code, side, price, size FROM %s WHERE product_code = ? ORDER BY time DESC LIMIT ?)
-	ORDER BY time ASC;`, tableNameSignalEvents)
+        SELECT time, product_code, side, price, size FROM %s WHERE product_code = ? ORDER BY time DESC LIMIT ? )
+        ORDER BY time ASC;`, tableNameSignalEvents)
 	rows, err := DbConnection.Query(cmd, config.Config.ProductCode, loadEvents)
 	if err != nil {
-		log.Println(err)
 		return nil
 	}
 	defer rows.Close()
@@ -52,22 +52,22 @@ func GetSignalEventsByCount(loadEvents int) *SignalEvents {
 	var signalEvents SignalEvents
 	for rows.Next() {
 		var signalEvent SignalEvent
-		err := rows.Scan(&signalEvent.Time, &signalEvent.ProductCode, &signalEvent.Side, &signalEvent.Price, &signalEvent.Size)
-		if err != nil {
-			log.Println(err)
-			return nil
-		}
+		rows.Scan(&signalEvent.Time, &signalEvent.ProductCode, &signalEvent.Side, &signalEvent.Price, &signalEvent.Size)
 		signalEvents.Signals = append(signalEvents.Signals, signalEvent)
+	}
+	err = rows.Err()
+	if err != nil {
+		return nil
 	}
 	return &signalEvents
 }
 
 func GetSignalEventsAfterTime(timeTime time.Time) *SignalEvents {
 	cmd := fmt.Sprintf(`SELECT * FROM (
-				SELECT time, product_code, side, price, size FROM %s
-				WHERE DATETIME(time) >= DATETIME(?)
-				ORDER BY time DESC
-			) ORDER BY time ASC;`, tableNameSignalEvents)
+                SELECT time, product_code, side, price, size FROM %s
+                WHERE DATETIME(time) >= DATETIME(?)
+                ORDER BY time DESC
+            ) ORDER BY time ASC;`, tableNameSignalEvents)
 	rows, err := DbConnection.Query(cmd, timeTime.Format(time.RFC3339))
 	if err != nil {
 		return nil
@@ -84,12 +84,12 @@ func GetSignalEventsAfterTime(timeTime time.Time) *SignalEvents {
 }
 
 func (s *SignalEvents) CanBuy(time time.Time) bool {
-	lenSignal := len(s.Signals)
-	if lenSignal == 0 {
+	lenSignals := len(s.Signals)
+	if lenSignals == 0 {
 		return true
 	}
 
-	lastSignal := s.Signals[lenSignal-1]
+	lastSignal := s.Signals[lenSignals-1]
 	if lastSignal.Side == "SELL" && lastSignal.Time.Before(time) {
 		return true
 	}
@@ -97,24 +97,24 @@ func (s *SignalEvents) CanBuy(time time.Time) bool {
 }
 
 func (s *SignalEvents) CanSell(time time.Time) bool {
-	lenSignal := len(s.Signals)
-	if lenSignal == 0 {
+	lenSignals := len(s.Signals)
+	if lenSignals == 0 {
 		return false
 	}
 
-	lastSignal := s.Signals[lenSignal-1]
+	lastSignal := s.Signals[lenSignals-1]
 	if lastSignal.Side == "BUY" && lastSignal.Time.Before(time) {
 		return true
 	}
 	return false
 }
 
-func (s *SignalEvents) Buy(productCode string, time time.Time, price, size float64, save bool) bool {
+func (s *SignalEvents) Buy(ProductCode string, time time.Time, price, size float64, save bool) bool {
 	if !s.CanBuy(time) {
 		return false
 	}
 	signalEvent := SignalEvent{
-		ProductCode: productCode,
+		ProductCode: ProductCode,
 		Time:        time,
 		Side:        "BUY",
 		Price:       price,
@@ -128,6 +128,7 @@ func (s *SignalEvents) Buy(productCode string, time time.Time, price, size float
 }
 
 func (s *SignalEvents) Sell(productCode string, time time.Time, price, size float64, save bool) bool {
+
 	if !s.CanSell(time) {
 		return false
 	}
@@ -170,7 +171,7 @@ func (s *SignalEvents) Profit() float64 {
 	return total
 }
 
-func (s *SignalEvents) MarshalJSON() ([]byte, error) {
+func (s SignalEvents) MarshalJSON() ([]byte, error) {
 	value, err := json.Marshal(&struct {
 		Signals []SignalEvent `json:"signals,omitempty"`
 		Profit  float64       `json:"profit,omitempty"`
@@ -181,7 +182,7 @@ func (s *SignalEvents) MarshalJSON() ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	return value, nil
+	return value, err
 }
 
 func (s *SignalEvents) CollectAfter(time time.Time) *SignalEvents {
--- bitCoinTradeSystem/tradingBitCoin/main.go
+++ bitCoinTradeSystem/tradingBitCoin/main.go
@@ -1,60 +1,55 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"tradingBitCoin/app/models"
+	"log"
+	"tradingBitCoin/app/controllers"
+	"tradingBitCoin/config"
+	"tradingBitCoin/utils"
 )
 
-//import (
-//	"tradingBitCoin/app/controllers"
-//	"tradingBitCoin/config"
-//	"tradingBitCoin/utils"
-//)
-//
-//func main() {
-//	utils.LoggingSettings(config.Config.LogFile)
-//	//apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-//	//ticker, _ := apiClient.GetTicker("BTC_USD")
-//	//fmt.Println(ticker)
-//	//fmt.Println(ticker.GetMidPrice())
-//	//fmt.Println(ticker.DateTime())
-//	//fmt.Println(ticker.TruncateDateTime(time.Hour))
-//
-//	//order := &bitflyer.Order{
-//	//	ProductCode:     config.Config.ProductCode,
-//	//	ChildOrderType:  "LIMIT",
-//	//	Side:            "BUY",
-//	//	Price:           7000,
-//	//	Size:            0.01,
-//	//	MinuteToExpires: 1,
-//	//	TimeInForce:     "GTC",
-//	//}
-//	//res, _ := apiClient.SendOrder(order)
-//	//fmt.Println(res.ChildOrderAcceptanceID)
-//
-//	//i := "JRF20181012-144016-140584"
-//	//params := map[string]string{
-//	//	"product_code": config.Config.ProductCode,
-//	//	"child_order_acceptance_id": i,
-//	//}
-//	//r, _ := apiClient.ListOrder(params)
-//	//fmt.Println(r)
-//
-//	// fmt.Println(models.DbConnection)
-//	controllers.StreamIngestionData()
-//	controllers.StartWebServer()
-//}
-
 func main() {
-	s := models.NewSignalEvents()
-	df, _ := models.GetAllCandle("BTC_USD", time.Minute, 10)
-	c1 := df.Candles[0]
-	c2 := df.Candles[5]
-	s.Buy("BTC_USD", c1.Time.UTC(), c1.Close, 1.0, true)
-	s.Sell("BTC_USD", c2.Time.UTC(), c2.Close, 1.0, true)
-	fmt.Println(models.GetSignalEventsByCount(1))
-	fmt.Println(models.GetSignalEventsAfterTime(c1.Time))
-	fmt.Println(s.CollectAfter(time.Now().UTC()))
-	fmt.Println(s.CollectAfter(c1.Time))
+	utils.LoggingSettings(config.Config.LogFile)
+	//apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+	//ticker, _ := apiClient.GetTicker("BTC_USD")
+	//fmt.Println(ticker)
+	//fmt.Println(ticker.GetMidPrice())
+	//fmt.Println(ticker.DateTime())
+	//fmt.Println(ticker.TruncateDateTime(time.Hour))
+
+	//order := &bitflyer.Order{
+	//	ProductCode:     config.Config.ProductCode,
+	//	ChildOrderType:  "LIMIT",
+	//	Side:            "BUY",
+	//	Price:           7000,
+	//	Size:            0.01,
+	//	MinuteToExpires: 1,
+	//	TimeInForce:     "GTC",
+	//}
+	//res, _ := apiClient.SendOrder(order)
+	//fmt.Println(res.ChildOrderAcceptanceID)
+
+	//i := "JRF20181012-144016-140584"
+	//params := map[string]string{
+	//	"product_code": config.Config.ProductCode,
+	//	"child_order_acceptance_id": i,
+	//}
+	//r, _ := apiClient.ListOrder(params)
+	//fmt.Println(r)
+
+	// fmt.Println(models.DbConnection)
+	controllers.StreamIngestionData()
+	log.Println(controllers.StartWebServer())
 }
+
+//func main() {
+//	s := models.NewSignalEvents()
+//	df, _ := models.GetAllCandle("BTC_USD", time.Minute, 10)
+//	c1 := df.Candles[len(df.Candles)-2]
+//	c2 := df.Candles[len(df.Candles)-1]
+//	s.Buy("BTC_USD", c1.Time.UTC(), c1.Close, 1.0, true)
+//	s.Sell("BTC_USD", c2.Time.UTC(), c2.Close, 1.0, true)
+//	fmt.Println(models.GetSignalEventsByCount(1))
+//	fmt.Println(models.GetSignalEventsAfterTime(c1.Time))
+//	fmt.Println(s.CollectAfter(time.Now().UTC()))
+//	fmt.Println(s.CollectAfter(c1.Time))
+//}
